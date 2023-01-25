3941d013c9fc25f29295892dcb17038ceb2b5a86
Kiny <89192734912nik@gmail.com>
2023-01-26 00:30:56 +0300

    init

---
+++ Milestone 7-9/Milestone 7-9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone 7-9
+//
+//  Created by nikita on 26.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
